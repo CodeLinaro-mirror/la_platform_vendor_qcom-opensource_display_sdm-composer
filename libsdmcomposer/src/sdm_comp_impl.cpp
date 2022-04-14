81833c36503180133094399d849077ba84170ce2
Ramkumar Radhakrishnan <quic_rradhakr@quicinc.com>
2022-04-14 16:18:05 -0700

    sdm-comp: Load the kernel module on sdm comp service start
    Load the kernel module during sdm_comp_service start instead of sdm
    core creation.
    
    Change-Id: I6aadfc341e3dc094b582d507c1722c49d5f03e0a

--- libsdmcomposer/src/sdm_comp_impl.cpp
+++ libsdmcomposer/src/sdm_comp_impl.cpp
@@ -57,7 +57,6 @@ OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <systemdq/sd-bus.h>
 #include <string.h>
 
 #include <thread>
@@ -71,9 +70,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define __CLASS__ "SDMCompImpl"
 
-#define DISPLAY_SERVICE_FILE "display@.service"
-#define SYSTEMD_ESCAPE_BIN "/bin/systemd-escape"
-
 namespace sdm {
 
 SDMCompImpl *SDMCompImpl::sdm_comp_impl_ = nullptr;
@@ -81,88 +77,6 @@ SDMCompDisplayBuiltIn *SDMCompImpl::display_builtin_[kSDMCompDisplayTypeMax] = {
 uint32_t SDMCompImpl::ref_count_ = 0;
 uint32_t SDMCompImpl::disp_ref_count_[kSDMCompDisplayTypeMax] = { 0 };
 recursive_mutex recursive_mutex_;
-
-static bool IsModuleLoaded() {
-  FILE *fp = popen("cat /proc/modules | grep msm_drm", "r");
-  if (fp == NULL) {
-    return false;
-  }
-  char buf[16];
-  if (fread (buf, 1, sizeof (buf), fp) > 0) {
-    pclose(fp);
-    return true;
-  }
-  pclose(fp);
-  return false;
-}
-
-static int LoadModule(const string &service_file_path, const string &argument) {
-  if (IsModuleLoaded()) {
-    return 0;
-  }
-  sd_bus_error error = SD_BUS_ERROR_NULL;
-  sd_bus_message *msg = NULL;
-  sd_bus *bus = NULL;
-  char systemd_escape_cmd[256] = {};
-  char modified_arg[256] = {};
-  string mod_service_file_path(service_file_path);
-  if (!argument.empty()) {
-    FILE *cmd_file;
-    snprintf(systemd_escape_cmd, sizeof(systemd_escape_cmd), "%s '%s' 2>&1",
-             SYSTEMD_ESCAPE_BIN, argument.c_str());
-    DLOGI("systemd_escape_cmd %s", systemd_escape_cmd);
-    cmd_file = popen(systemd_escape_cmd, "r");
-    if (!cmd_file) {
-      DLOGI("cmd file is NULL");
-      return -EINVAL;
-    }
-    if (fgets(modified_arg, sizeof(modified_arg), cmd_file) == NULL) {
-      DLOGE("fgets returned null");
-      return -EINVAL;
-    }
-    DLOGI("modified_arg %s", modified_arg);
-    pclose(cmd_file);
-
-    std::size_t pos = mod_service_file_path.find_first_of('@');
-    if (pos == string::npos) {
-      DLOGE("Invalid service file path!!");
-      return -EINVAL;
-    }
-    mod_service_file_path.insert(pos + 1, modified_arg);
-
-    pos = mod_service_file_path.find_first_of('\n');
-    if (pos != string::npos) {
-      mod_service_file_path.erase(pos, 1);
-    }
-  }
-
-  DLOGI("service file path %s", mod_service_file_path.c_str());
-
-  int err = sd_bus_open_system(&bus);
-  if (err < 0) {
-    DLOGE("Failed to connect to system bus err %d %s", err, strerror(errno));
-    return err;
-  }
-  err = sd_bus_call_method(bus,
-                           "org.freedesktop.systemd1",
-                           "/org/freedesktop/systemd1",
-                           "org.freedesktop.systemd1.Manager",
-                           "StartUnit",
-                           &error,
-                           &msg,
-                           "ss",
-                           mod_service_file_path.c_str(),
-                           "replace");
-  if (err < 0) {
-    DLOGE("sd_bus_call_method failed with err %d %s", err, strerror(errno));
-  }
-  sd_bus_error_free(&error);
-  sd_bus_message_unref(msg);
-  sd_bus_unref(bus);
-
-  DLOGI("Loading kernel module is %s", (err < 0) ? "failed" : "successful");
-  return ((err < 0) ? err : 0);
-}
 
 void SDMCompImpl::CoreInterfaceCb(CoreInterface *obj)
 {
@@ -188,18 +102,6 @@ int SDMCompImpl::Init() {
   if (ret != 0) {
     DLOGW("SDMCompServiceIntf create failed!! %d\n", ret);
     return ret;
-  }
-
-  if (!IsModuleLoaded()) {
-    std::unique_lock<std::mutex> lck(mutex_panel_boot_params_);
-    while (panel_boot_params_updated_ == false) {
-      cv_panel_boot_params_.wait(lck);
-    }
-    ret = LoadModule(DISPLAY_SERVICE_FILE, panel_boot_params_);
-    if (ret != 0) {
-      DLOGE("Failed loading kernel module %d", ret);
-      return ret;
-    }
   }
 
   ipc_intf_ = std::make_shared<SDMCompIPCImpl>();
@@ -458,14 +360,6 @@ int SDMCompImpl::OnEvent(SDMCompServiceEvents event, ...) {
       if (demura_buf_info->calib_buf_fd > 0)
         std::thread(CoreInterfaceCb, nullptr).detach();
     }
-  } break;
-
-  case kEventSetPanelBootParams: {
-    std::unique_lock<std::mutex> lck(mutex_panel_boot_params_);
-    panel_boot_params_ = std::string(reinterpret_cast<const char *>(va_arg(arguments, Handle)));
-    DLOGI("Panel boot params %s", panel_boot_params_.c_str());
-    panel_boot_params_updated_ = true;
-    cv_panel_boot_params_.notify_one();
   } break;
 
   default:
